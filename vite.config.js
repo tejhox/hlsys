cad22d66be42d1782f844c238d03570da4ce66b5
tejhox <bowodwi1996@gmail.com>
2025-07-20 03:21:09 +0700

    Fix vite base config and rebuild assets

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        assetsDir: 'assets',
+    },
+    base: '/build/', 
 });
