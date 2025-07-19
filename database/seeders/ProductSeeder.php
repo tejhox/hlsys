c7d2cf3e37e314083872714e3ec69339e9f631bb
tejhox <bowodwi1996@gmail.com>
2025-07-20 03:07:39 +0700

    Include vite build assets and other fixes

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -13,12 +13,19 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {   
-        Product::truncate();
+        // Nonaktifkan foreign key checks sementara supaya aman hapus data
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Product::query()->delete();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
-        $products = ['KS Reguler', 'KS Cross', 'D91/92L', 'D26A H', 'D26A M', 'D26A L', 'D14N', 'D02A', 'D55L'];
+        $products = ['KS Reguler', 'KS Cross', 'D91/92L', 'D26A H', 'D26A M', 'D26A L', 'D14N', 'D02A', 'D55L', 'D52 M', 'D52 H'];
+        $cycle_time = [1.5, 1.5, 1.33, 1.09, 1.09, 1.09, 1.09, 1.09, 1.09, 1.09, 1.09];
 
-        foreach ($products as $product) {
-            Product::create(['name' => $product]);
+        foreach ($products as $index => $product) {
+            Product::create([
+                'name' => $product,
+                'cycle_time' => $cycle_time[$index]
+            ]);
         }
     }
 }
