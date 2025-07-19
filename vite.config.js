0efbdec3b361da7f58671e69db8a153c38a321c1
tejhox <bowodwi1996@gmail.com>
2025-07-20 03:45:47 +0700

    Fix: move manifest.json to root build folder

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,19 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    base: '/build/',
     build: {
         manifest: true,
         outDir: 'public/build',
         assetsDir: 'assets',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash][extname]',
+            },
+        },
     },
-    base: '/build/', 
 });
